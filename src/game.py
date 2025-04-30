1975f4e2cf9ad05c7cfdd8aa81f99f805d82f066
yutingtai <qsid1130@gmail.com>
2025-04-30 14:45:45 +0800

    use transparent png picture as player

--- src/game.py
+++ src/game.py
@@ -4,7 +4,6 @@ from player import Player
 from sprites import sprites
 
 
-
 def start_game(running: bool, player):
     while running:
         for event in pygame.event.get():
@@ -14,12 +13,12 @@ def start_game(running: bool, player):
                 keys_down.add(event.key)
             elif event.type == pygame.KEYUP:
                 keys_down.remove(event.key)
-    
+
         # update code
 
         player.update()
-        
-        # draw code 
+
+        # draw code
         screen.fill(clear_color)
         for s in sprites:
             s.draw(screen)
@@ -28,12 +27,11 @@ def start_game(running: bool, player):
     pygame.quit()
 
 
-
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption("Adeventure Game")
-    screen = pygame.display.set_mode((600,800))
+    screen = pygame.display.set_mode((600, 800))
     clear_color = (30, 120, 50)
-    player = Player("images/test.png", 0 , 0)
+    player = Player("images/patrik.png", 0, 0)
     running = True
     start_game(running, player)
